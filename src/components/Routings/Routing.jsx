8f86641986e75f9719a81527d31de6bc8f48d144
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-05 19:52:11 -0400

    feat: error page

--- src/components/Routings/Routing.jsx
+++ src/components/Routings/Routing.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Kakao from 'pages/SocialLogIn/Kakao';
-import Google from 'pages/SocialLogIn/Google';
-import SignUp from 'pages/SignUp';
-import LogIn from 'pages/LogIn';
-import Error from 'pages/Status/Error';
-import ProtectedRoutesNoLogin from './ProtectedNoLogin';
-import Home from 'pages/Home';
-import GoalShot from 'pages/GoalShot';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Kakao from "pages/SocialLogIn/Kakao";
+import Google from "pages/SocialLogIn/Google";
+import SignUp from "pages/SignUp";
+import LogIn from "pages/LogIn";
+import Error from "pages/Status/Error";
+import ProtectedRoutesNoLogin from "./ProtectedNoLogin";
+import Home from "pages/Home";
+import GoalShot from "pages/GoalShot";
 
 const Routing = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<LogIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/oauth/kakao/callback" element={<Kakao />} />
-            <Route path="/goalshot" element={<GoalShot />} />
-            <Route path="/oauth/google/callback" element={<Google />} />
+  return (
+    <Routes>
+      <Route path="/" element={<Error />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/oauth/kakao/callback" element={<Kakao />} />
+      <Route path="/goalshot" element={<GoalShot />} />
+      <Route path="/oauth/google/callback" element={<Google />} />
 
-            <Route element={<ProtectedRoutesNoLogin />}>
-                <Route path="/home" element={<Home />} />
-            </Route>
+      <Route element={<ProtectedRoutesNoLogin />}>
+        <Route path="/home" element={<Home />} />
+      </Route>
 
-            <Route path="*" element={<Error />} />
-        </Routes>
-    );
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 };
 
 export default Routing;
--- src/pages/Status/Error.jsx
+++ src/pages/Status/Error.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { StWrap } from "components/Common/GlobalStyles";
+import styled from "@emotion/styled";
+import flex from "components/Common/flex";
+import Button from "components/Common/Button";
 
 const Error = () => {
-  return <div>Error....</div>;
+  return (
+    <StWrapFlex>
+      <span className="bigSpan">앗!</span>
+      <span className="smallSpan">오류가 발생했어요. </span>
+      <Button text="뒤로가기" theme="dark" />
+    </StWrapFlex>
+  );
 };
 
 export default Error;
+
+const StWrapFlex = styled(StWrap)`
+  ${flex({ direction: "column" })}
+  .bigSpan {
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 130%;
+    margin-bottom: 6px;
+  }
+  .smallSpan {
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 130%;
+    margin-bottom: 42px;
+  }
+`;
