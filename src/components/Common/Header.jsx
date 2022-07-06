7f61a74f97cc0b0ba33a2caa7383343090085cf4
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-06 08:34:21 -0400

    feat: Header View

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,68 +1,63 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { Outlet } from 'react-router';
+import React from "react";
+import styled from "@emotion/styled";
+import { StWrap } from "./GlobalStyles";
+import Dog from "assets/img/Dog.png";
+import SmallMenu from "assets/img/smallMenu.png";
+import flex from "./flex";
 
 const Header = () => {
-    return (
-        <HeaderWrap>
-            <ImageWrap>
-                <Image>{/* <image /> */}</Image>
-                <NicknameWrap>
-                    <Laval>갓생 입문자</Laval>
-                    <Nickname>김갓생</Nickname>
-                </NicknameWrap>
-            </ImageWrap>
-            <NavigatorBar>...</NavigatorBar>
-            <Outlet />
-        </HeaderWrap>
-    );
+  return (
+    <StWrapFlex>
+      <HeaderWrap>
+        <div className="LeftDiv">
+          <img className="ProfilePic" src={Dog} alt="" />
+          <div className="textDiv">
+            <span className="secondaryText">갓생 입문자</span>
+            <span>김갓생</span>
+          </div>
+        </div>
+        <StImg src={SmallMenu} alt="" />
+      </HeaderWrap>
+    </StWrapFlex>
+  );
 };
 
 export default Header;
 
+const StWrapFlex = styled(StWrap)`
+  ${flex({ align: "flex-start" })}
+  margin-top: 60px;
+`;
+
 const HeaderWrap = styled.div`
-    width: 310px;
-    height: 80px;
-    /* display: flex;
-    justify-items: row;
-    justify-content: flex-start;
-    position: absolute; */
-    flex-direction: row;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 15vh;
-    background-color: white;
-    color: transparent;
-`;
-const ImageWrap = styled.div`
-    flex-direction: row;
-    display: flex;
-    overflow: hidden;
-    margin-left: 33px;
-`;
-const Image = styled.div`
+  ${flex({ justify: "space-between" })}
+  width: 310px;
+  height: 80px;
+  .LeftDiv {
+    ${flex({})}
+  }
+  .ProfilePic {
     width: 80px;
     height: 80px;
-    border-radius: 80px;
-
-    background-color: black;
-`;
-const NicknameWrap = styled.div`
-    flex-direction: column;
-    justify-content: center;
-`;
-const Laval = styled.p`
-    color: gray;
-    font-size: 13px;
-    text-align: center;
-`;
-const Nickname = styled.div`
-    color: black;
+    border-radius: 60px;
+  }
+  .textDiv {
+    ${flex({ direction: "column", align: "flex-start" })}
+    font-weight: 700;
     font-size: 18px;
+    line-height: 130%;
+    margin-left: 7.5px;
+    color: #212121;
+    .secondaryText {
+      font-weight: 400;
+      font-size: 13px;
+      line-height: 130%;
+      color: #888888;
+    }
+  }
 `;
-const NavigatorBar = styled.div`
-    margin-right: 13px;
-    color: black;
+
+const StImg = styled.img`
+  width: 3px;
+  height: 12px;
 `;
