fc02c660ef16dc54dd61303de3e99f6c2122495e
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-12 15:24:54 -0400

    feat: like - add function

--- src/components/Hooks/useAddLike.jsx
+++ src/components/Hooks/useAddLike.jsx
@@ -1,21 +1,21 @@
-import { useMutation, useQueryClient } from 'react-query';
-import apis from 'shared/api/main';
+import { useMutation, useQueryClient } from "react-query";
+import apis from "shared/api/main";
 
 const addLike = async (payload) => {
-    const addLikeDB = await apis.addLike(payload);
-    return addLikeDB;
+  const addLikeDB = await apis.addLike(payload);
+  return addLikeDB;
 };
 const useAddLike = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    return useMutation(addLike, {
-        onSuccess: () => {
-            queryClient.invalidateQueries('like');
-        },
-        onError: (e) => {
-            alert(e.massage);
-        },
-    });
+  return useMutation(addLike, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries("like");
+    },
+    onError: (e) => {
+      alert(e.massage);
+    },
+  });
 };
 
 export default useAddLike;
--- src/shared/api/main.js
+++ src/shared/api/main.js
@@ -1,34 +1,43 @@
-import api from './core';
+import api from "./core";
 
 export const apis = {
-    // user
-    signUp: (payload) => api.put('/user/update/nickname', payload),
-    dupCheck: (payload) => api.post('/user/signup/checkNickname', payload),
-    newOldUser: () => api.put('/user/update/isNewUser'),
+  // user
+  signUp: (payload) => api.put("/user/update/nickname", payload),
+  dupCheck: (payload) => api.post("/user/signup/checkNickname", payload),
+  newOldUser: () => api.put("/user/update/isNewUser"),
 
-    getUser: () => api.get('/user/auth'),
-    updateUserNickname: (payload) => api.put('/user/update/nickname', payload),
-    updateUserProfile: (payload) => api.put('/user/update/profile', payload),
+  getUser: () => api.get("/user/auth"),
+  updateUserNickname: (payload) => api.put("/user/update/nickname", payload),
+  updateUserProfile: (payload) => api.put("/user/update/profile", payload),
 
-    // mission CRUD
-    getTodos: () => api.get('/api/category/missions'),
-    addTodo: (payload) => api.post('api/category/missions', payload),
-    updateTodo: (payload) => api.patch(`/todos/${payload.id}`, payload),
-    completedTodo: (payload) => api.put(`/api/missions/changeMissionState/${payload.missionId}`, {}),
-    deleteTodo: (payload) => api.delete(`/todos/${payload.id}`),
+  // mission CRUD
+  getTodos: () => api.get("/api/category/missions"),
+  addTodo: (payload) => api.post("api/category/missions", payload),
+  updateTodo: (payload) => api.patch(`/todos/${payload.id}`, payload),
+  completedTodo: (payload) =>
+    api.put(`/api/missions/changeMissionState/${payload.missionId}`, {}),
+  deleteTodo: (payload) => api.delete(`/todos/${payload.id}`),
 
-    // post CRUD
-    addPost: (payload) => api.post('/api/post', payload),
-    getPost: () => api.get('/api/posts'),
-    getPostDetail: (payload) => api.get(`/api/post/${payload.postId}`, payload),
-    deletePost: (payload) => api.delete(`/api/post/${payload.postId}`, payload),
+  // post CRUD
+  addPost: (payload) => api.post("/api/post", payload),
+  getPost: () => api.get("/api/posts"),
+  getPostDetail: (payload) => api.get(`/api/post/${payload.postId}`, payload),
+  deletePost: (payload) => api.delete(`/api/post/${payload.postId}`, payload),
 
-    // comment CRUD
-    likeComment: (payload) => api.post(`/api/post/${payload.postId}/like`, payload),
-    getComment: (payload) => api.get(`/api/post/${payload.postId}/comment`),
-    addComment: (payload) => api.post(`/api/post/${payload.postId}/comment`, payload),
-    updateComment: (payload) => api.patch(`/api/post/${payload.postId}/comment/${payload.commentId}`, payload),
-    deleteComment: (payload) => api.delete(`/api/post/${payload.postId}/comment/${payload.commentId}`),
+  // comment CRUD
+  getComment: (payload) => api.get(`/api/post/${payload.postId}/comment`),
+  addComment: (payload) =>
+    api.post(`/api/post/${payload.postId}/comment`, payload),
+  updateComment: (payload) =>
+    api.patch(
+      `/api/post/${payload.postId}/comment/${payload.commentId}`,
+      payload
+    ),
+  deleteComment: (payload) =>
+    api.delete(`/api/post/${payload.postId}/comment/${payload.commentId}`),
+
+  // like CRUD
+  addLike: (payload) => api.post(`/api/post/${payload.postId}/like`, {}),
 };
 
 export default apis;
