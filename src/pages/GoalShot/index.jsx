b89e3cfdc2e5aa7d4f91d8899685ab22595eff02
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-13 20:34:58 -0400

    feat: Goal Shot Page - Like Swiping without CSS

--- src/pages/GoalShot/index.jsx
+++ src/pages/GoalShot/index.jsx
@@ -1,11 +1,55 @@
 import { StWrap } from "components/Common/GlobalStyles";
 import NavigatorBar from "components/Common/NavigatorBar";
 import styled from "@emotion/styled";
+import flex from "components/Common/flex";
+import useGetPost from "components/Hooks/useGetPost";
+import { useState } from "react";
+import useAddLike from "components/Hooks/Like/useAddLike";
 
 const GoalShot = () => {
+  const [count, setCount] = useState(1);
+  const { data } = useGetPost();
+  const { mutate } = useAddLike();
+  console.log(data);
+
+  const addLike = () => {
+    mutate({
+      postId: data[count]?.postId,
+    });
+  };
+
+  const onLike = () => {
+    if (count < data?.length - 1) {
+      setCount((value) => value + 1);
+      addLike();
+    } else {
+      return;
+    }
+  };
+  const onDisLike = () => {
+    if (count > -1) {
+      setCount((value) => value - 1);
+    } else {
+      return;
+    }
+  };
   return (
     <>
-      <StWrapFlex></StWrapFlex>
+      <StWrapFlex>
+        <StCardContainer>
+          {data !== undefined ? (
+            <>
+              <StCard img={data[count - 1]?.photoUrl} differ="40" />
+              <StCard img={data[count]?.photoUrl} />
+              <StCard img={data[count + 1]?.photoUrl} differ="40" />
+            </>
+          ) : (
+            <></>
+          )}
+        </StCardContainer>
+        <button onClick={onLike}>좋아요</button>
+        <button onClick={onDisLike}>싫어요</button>
+      </StWrapFlex>
       <NavigatorBar />
     </>
   );
@@ -13,4 +57,24 @@ const GoalShot = () => {
 
 export default GoalShot;
 
-const StWrapFlex = styled(StWrap)``;
+const StWrapFlex = styled(StWrap)`
+  ${flex({ direction: "column" })}
+  overflow: hidden;
+  width: 100%;
+`;
+
+const StCardContainer = styled.div`
+  ${flex({ gap: "10px" })}
+  width: 100%;
+`;
+
+const StCard = styled.div`
+  min-width: 285px;
+  height: 480px;
+  background-image: url(${(props) => props.img});
+  background-position: center;
+  background-size: cover;
+  filter: drop-shadow(0px 43px 40px rgba(0, 0, 0, 0.2));
+  border-radius: 20px;
+  margin-top: ${(props) => props.differ}px;
+`;
