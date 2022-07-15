2aeaff6fc041eeae3b9d28909b78fca560bafca4
baeg-jae <105036532+baeg-jae@users.noreply.github.com>
2022-07-16 06:14:34 +0900

    fetch.머지합니다

--- src/components/Common/GlobalStyles.jsx
+++ src/components/Common/GlobalStyles.jsx
@@ -1,44 +1,44 @@
-import styled from "@emotion/styled";
-import { keyframes } from "@emotion/react";
-import flex from "./flex";
-import "./font.css";
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+import flex from './flex';
+import './font.css';
 
 export const FlexRowDiv = styled.div`
-  ${flex({})}
+    ${flex({})}
 `;
 
 export const FlexColumnDiv = styled.div`
-  ${flex({ direction: "column" })}
+    ${flex({ direction: 'column' })}
 `;
 
 export const StWrap = styled.div`
-  width: 100%;
-  height: 100vh;
+    width: 100%;
+    height: 100vh;
 `;
 
 export const StInput = styled.input`
-  font-size: 16px;
-  &::placeholder {
-    color: #b7b7b7;
     font-size: 16px;
-  }
+    &::placeholder {
+        color: #b7b7b7;
+        font-size: 16px;
+    }
 `;
 
 export const StWrapFlex = styled(StWrap)`
-  ${flex({ direction: "column" })}
-  color: #665547;
-  .bigSpan {
-    font-weight: 700;
-    font-size: 64px;
-    line-height: 130%;
-    margin-bottom: 6px;
-  }
-  .smallSpan {
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 130%;
-    margin-bottom: 42px;
-  }
+    ${flex({ direction: 'column' })}
+    color: #665547;
+    .bigSpan {
+        font-weight: 700;
+        font-size: 64px;
+        line-height: 130%;
+        margin-bottom: 6px;
+    }
+    .smallSpan {
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 130%;
+        margin-bottom: 42px;
+    }
 `;
 
 export const fadeAnimation = keyframes`
