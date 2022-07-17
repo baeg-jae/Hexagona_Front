7bf58f0f1a12cbcf650ad8429e1357f54124d05e
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-16 23:10:08 -0400

    fix: css in mypage / dark mode

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -9,17 +9,17 @@ import ReactSwitch from "react-switch";
 export const ThemeContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState("light");
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <Fullscreen className="App" id={theme}>
-        <div className="switch">
+        {/* <div className="switch">
           <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
           <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
-        </div>
+        </div> */}
         <Wrap>
           <MobileFrame>
             <Routing />
