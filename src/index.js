98761796d0d996cf321b8e2d1fa7e7869321e658
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-17 20:33:19 -0400

    feat: Mobile Setting

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from 'layouts/App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "layouts/App";
+import { BrowserRouter } from "react-router-dom";
+import { BrowserView, MobileView } from "react-device-detect";
+import "./index.css";
 // React-Query
-import { QueryClient, QueryClientProvider } from 'react-query';
-// import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 // Redux
-import { Provider } from 'react-redux';
-import store from 'redux/configstore';
+import { Provider } from "react-redux";
+import store from "redux/configstore";
+import Mobile from "layouts/Mobile";
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-            {/* <ReactQueryDevtools initialIsOpen /> */}
-        </QueryClientProvider>
-    </Provider>
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <BrowserView>
+          <App />
+        </BrowserView>
+        <MobileView>
+          <Mobile />
+        </MobileView>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </Provider>
 );
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('/serviceworker.js')
-            .then((registration) => {})
-            .catch((e) => {});
-    });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceworker.js")
+      .then((registration) => {})
+      .catch((e) => {});
+  });
 }
---
+++ src/layouts/Mobile.jsx
@@ -0,0 +1,12 @@
+import MobileFrame from "components/Common/MobileFrame";
+import Routing from "components/Routings/Routing";
+
+const Mobile = () => {
+  return (
+    <MobileFrame>
+      <Routing />
+    </MobileFrame>
+  );
+};
+
+export default Mobile;
