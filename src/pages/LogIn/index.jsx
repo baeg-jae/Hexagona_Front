4398fd3a634253541952478bfde7ced3eb42fd6a
baeg-jae <105036532+baeg-jae@users.noreply.github.com>
2022-07-19 22:00:37 +0900

    naver로그인

--- src/pages/LogIn/index.jsx
+++ src/pages/LogIn/index.jsx
@@ -1,4 +1,4 @@
-import { KAKAO_AUTH_URL, GOOGLE_AUTH_URL } from 'shared/socialAuth';
+import { KAKAO_AUTH_URL, GOOGLE_AUTH_URL, NAVER_AUTH_URL } from 'shared/socialAuth';
 import { SocialButton } from 'components/SignIn/SocialButton';
 import LoginImg from 'assets/img/LoginImg.webp';
 import flex from 'components/Common/flex';
@@ -11,7 +11,7 @@ const LogIn = () => {
                 <span>이번엔 꼭 내 인생을 갓생으로</span>
                 <span className="bigFont">갓생메이커</span>
                 <SocialButton link={KAKAO_AUTH_URL} name="kakao" text="카카오 계정으로 시작하기" />
-                <SocialButton name="naver" text="네이버 계정으로 시작하기" />
+                <SocialButton link={NAVER_AUTH_URL} name="naver" text="네이버 계정으로 시작하기" />
                 <SocialButton link={GOOGLE_AUTH_URL} name="" text="Google 계정으로 계속하기" />
             </div>
         </StFirstPageDiv>
@@ -21,28 +21,23 @@ const LogIn = () => {
 export default LogIn;
 
 const StFirstPageDiv = styled.div`
-  ${flex({ direction: "column", justify: "flex-end" })}
-  width: 100%;
-  height: 100%;
-  font-size: 20px;
-  color: var(--white);
-  text-align: center;
-  background-size: contain;
-  background-image: url(${LoginImg});
-  .bigFont {
-    font-weight: 700;
-    font-size: 48px;
-    margin-bottom: 13px;
-  }
-  .gradient {
-    ${flex({ direction: "column", justify: "flex-end" })}
+    ${flex({ direction: 'column', justify: 'flex-end' })}
     width: 100%;
-    height: 50%;
-    background: linear-gradient(
-      180deg,
-      rgba(30, 5, 5, 0) -9.31%,
-      var(--gradient) 62.46%
-    );
-  }
-
+    height: 100%;
+    font-size: 20px;
+    color: var(--white);
+    text-align: center;
+    background-size: contain;
+    background-image: url(${LoginImg});
+    .bigFont {
+        font-weight: 700;
+        font-size: 48px;
+        margin-bottom: 13px;
+    }
+    .gradient {
+        ${flex({ direction: 'column', justify: 'flex-end' })}
+        width: 100%;
+        height: 50%;
+        background: linear-gradient(180deg, rgba(30, 5, 5, 0) -9.31%, var(--gradient) 62.46%);
+    }
 `;
