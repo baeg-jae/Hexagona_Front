9a336d96333c6fb864c5e8de1b1ccb9d6c3f50f6
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-21 12:56:20 -0400

    fix: goal shot button errror

--- src/components/GoalShot/GoalShotButtons.jsx
+++ src/components/GoalShot/GoalShotButtons.jsx
@@ -15,9 +15,10 @@ const GoalShotButtons = ({
 }) => {
   const disableHandler = () => {
     if (chooseOne || chooseTwo) return true;
+    if (data?.length < 11) return true;
     return false;
   };
-
+  console.log(data?.length);
   useEffect(() => {
     const interval = setTimeout(() => {
       if (chooseOne) {
