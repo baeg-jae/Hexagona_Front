ba1c5d1b6b12e23700eaca74d3f19a390f360410
baeg-jae <105036532+baeg-jae@users.noreply.github.com>
2022-07-26 08:00:58 +0900

    아직 작동x중간머지

--- src/components/Chat/Chatting.jsx
+++ src/components/Chat/Chatting.jsx
@@ -1,21 +1,28 @@
-// import SockJS from 'sockjs-client';
-// import Stomp from 'stompjs';
-// import React, { useEffect, useState } from 'react';
+import React from 'react';
 // import useGetUser from 'components/Hooks/User/useGetUser';
+// import useGetChatListSetting from 'components/Hooks/ChatList/useGetChatListSetting';
+// import useCreateChatRoom from 'components/Hooks/ChatList/useCreateChatRoom';
+import NewLikeImg from './NewLikeImg';
 
-// //골샷 아이 라잌, 채팅리스트 가져오기
-// //1  api 연결해준다.
-// //2. api main작성 이름은 getChatSetting
-// //3. 쿼리로 쓸꺼면 저희는 따로 쿼리 커스텀훅 만들기
-// //4. 커스텀훅 적용시키기
-// //5. 컴토넌트 만들기
+//골샷 아이 라잌, 채팅리스트 가져오기
+//1  api 연결해준다.
+//2. api main작성 이름은 getChatSetting
+//3. 쿼리로 쓸꺼면 저희는 따로 쿼리 커스텀훅 만들기
+//4. 커스텀훅 적용시키기
+//5. 컴토넌트 만들기
 
-// const Chatting = () => {
-//     const { data } = useGetUser();
+//문제점
+//usemutation 하면
 
-//     console.log(data);
+const Chatting = () => {
+    // const { data } = useGetUser();
+    // const { mutate, data } = useCreateChatRoom();
 
-//     return <div>Chatting</div>;
-// };
+    return (
+        <div>
+            <NewLikeImg />
+        </div>
+    );
+};
 
-// export default Chatting;
+export default Chatting;
--- src/components/Chat/NewLikeImg.jsx
+++ src/components/Chat/NewLikeImg.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
+import useGetUser from 'components/Hooks/User/useGetUser';
 
 const NewLikeImg = () => {
-  return (
-    <div>NewLikeImg</div>
-  )
-}
+    const { data } = useGetUser();
+    console.log(data);
+    return (
+        <StNewLikeImgWrap>
+            <StGoalShotILike>Goal Shot I Like</StGoalShotILike>
+            <StNewLikeImgList />
+        </StNewLikeImgWrap>
+    );
+};
 
-export default NewLikeImg
+export default NewLikeImg;
+
+const StNewLikeImgWrap = styled.div``;
+const StGoalShotILike = styled.div``;
+const StNewLikeImgList = styled.div``;
