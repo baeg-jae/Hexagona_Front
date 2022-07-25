6baa081b7885af5c4a508b3a7b872a1a74b6e30e
baeg-jae <105036532+baeg-jae@users.noreply.github.com>
2022-07-26 07:59:52 +0900

    ing아직미작동

--- src/pages/SocialLogIn/Google.jsx
+++ src/pages/SocialLogIn/Google.jsx
@@ -1,31 +1,31 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { __googleSignIn } from "redux/modules/user";
-import { useEffect } from "react";
-import loadable from "@loadable/component";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { __googleSignIn } from 'redux/modules/user';
+import { useEffect } from 'react';
+import loadable from '@loadable/component';
 
-const Loading = loadable(() => import("pages/Status/Loading"));
+const Loading = loadable(() => import('pages/Status/Loading'));
 
 const Google = () => {
-  const code = new URL(window.location.href).searchParams.get("code");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { isLogin, newUser } = useSelector((state) => state.userReducer);
-
-  useEffect(() => {
-    dispatch(__googleSignIn(code));
-    if (isLogin) {
-      navigate("/signup");
-    }
-    if (!newUser) {
-      navigate("/home");
-    }
-  }, [dispatch, code, navigate, isLogin, newUser]);
-  return (
-    <>
-      <Loading />
-    </>
-  );
+    const code = new URL(window.location.href).searchParams.get('code');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { isLogin, newUser } = useSelector((state) => state.userReducer);
+    console.log(code);
+    useEffect(() => {
+        dispatch(__googleSignIn(code));
+        if (isLogin) {
+            navigate('/signup');
+        }
+        if (!newUser) {
+            navigate('/home');
+        }
+    }, [dispatch, code, navigate, isLogin, newUser]);
+    return (
+        <>
+            <Loading />
+        </>
+    );
 };
 
 export default Google;
