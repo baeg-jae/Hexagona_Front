5105215b2f77669e05c213fdd4f525207cabd449
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-26 11:15:33 -0400

    feat: Chat Room Page - Send Message and Empty the Input

--- src/components/Chat/ChatSubscribe.jsx
+++ src/components/Chat/ChatSubscribe.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import useGetChatSetting from "components/Hooks/ChatList/useGetChatSetting";
@@ -7,15 +7,22 @@ import SockJS from "sockjs-client";
 import flex from "components/Common/flex";
 import styled from "@emotion/styled";
 import BackButton from "components/Common/BackButton";
-import temp from "assets/img/Moon.webp";
 import StChatContainer from "./StChatContainer";
 import { FlexColumnDiv } from "components/Common/GlobalStyles";
+import { useRef } from "react";
 
 const ChatSubscribe = () => {
   const { chatRoomId } = useParams();
   const SockJs = new SockJS(`${process.env.REACT_APP_SERVER}/ws-stomp`);
   const StompClient = Stomp.over(SockJs);
   const { userId } = useSelector((state) => state.userReducer);
+  const [message, setMessage] = useState("");
+  const inputRef = useRef();
+
+  // 지금 쓰는 채팅 가져오기
+  const addMessage = (e) => {
+    setMessage(e.target.value);
+  };
 
   // 이전 채팅 메시지 가져오는 query
   const { data } = useGetChatSetting({
@@ -53,6 +60,7 @@ const ChatSubscribe = () => {
     [StompClient.ws.readyState]
   );
 
+  // 채팅 재연결 코드
   const wsDisconnect = useCallback(() => {
     StompClient.disconnect(() => {
       // 구독할떄 콘솔보면 id:sub-0 가 이래서 비구독은 아래처럼
@@ -67,7 +75,24 @@ const ChatSubscribe = () => {
     // return () => wsDisconnect();
   }, [wsSubscribe, wsDisconnect]);
 
-  // 채팅 재연결 코드
+  // 버튼으로 채팅 보내기
+  const SendMessage = () => {
+    if (message === "") {
+      // 빈 메시지 칸 처리
+      return;
+    }
+    const data = {
+      userId: userId,
+      chatRoomId: chatRoomId,
+      modifiedAt: "2022-07-24T11:12:42.032",
+      message: message,
+    };
+
+    waitForConnect(StompClient, () => {
+      StompClient.send(`/pub/templates/chat/message`, {}, JSON.stringify(data));
+    });
+    inputRef.current.value = "";
+  };
 
   return (
     <FlexColumnDiv>
@@ -78,7 +103,21 @@ const ChatSubscribe = () => {
       </StHeader>
       <StBody>
         <StChatContainer />
-        <StInputDiv></StInputDiv>
+        <StInputDiv>
+          <StMyProfile img={data?.otherProfileImg} />
+          <StDiv>
+            <input
+              type="text"
+              className="commentInput"
+              placeholder="메세지를  입력해주세요."
+              onChange={addMessage}
+              ref={inputRef}
+            />
+            <button className="commentButton" onClick={SendMessage}>
+              게시
+            </button>
+          </StDiv>
+        </StInputDiv>
       </StBody>
     </FlexColumnDiv>
   );
@@ -111,12 +150,18 @@ const StOtherProfile = styled.div`
   background-position: center;
 `;
 
+const StMyProfile = styled(StOtherProfile)`
+  width: 40px;
+  height: 40px;
+  margin-left: 0px;
+`;
+
 const StInputDiv = styled.div`
+  ${flex({ justify: "flex-start" })}
   position: fixed;
   width: calc(100% - 32px);
   height: 50px;
   top: calc(100vh - 118px);
-  background-color: yellow;
 
   @media screen and (min-width: 420px) {
     width: 384px;
@@ -129,4 +174,35 @@ const StOtherName = styled.span`
   line-height: 130%;
   margin-left: 8px;
   color: #212121;
+`;
+
+const StDiv = styled.div`
+  ${flex({ justify: "space-around" })}
+  width: 100%;
+  height: 40px;
+  margin-left: 12px;
+  border: 1px solid #bfbfbf;
+  border-radius: 55px;
+  .commentInput {
+    width: calc(100% - 100px);
+    border: none;
+    background-color: transparent;
+    white-space: normal;
+    text-align: justify;
+    margin-left: 10px;
+    &:focus {
+      outline: none;
+    }
+    &::placeholder {
+      font-size: 11px;
+    }
+  }
+  .commentButton {
+    background-color: transparent;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 20px;
+    color: #4876ef;
+    margin-right: 15px;
+  }
 `;
