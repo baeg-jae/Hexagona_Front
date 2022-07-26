329097934d8a1865ad9decb3c5aa43c31e566538
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-26 08:34:02 -0400

    feat: Chat Page - If there is no I liked posts,

--- src/components/Chat/TodayLiked.jsx
+++ src/components/Chat/TodayLiked.jsx
@@ -2,6 +2,8 @@ import GetTodayLikes from "components/Hooks/Like/useTodayLikes";
 import styled from "@emotion/styled";
 import useCreateChatRoom from "components/Hooks/ChatList/useCreateChatRoom";
 import Swal from "sweetalert2";
+import flex from "components/Common/flex";
+import { HiCursorClick } from "react-icons/hi";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper";
@@ -9,6 +11,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "../GoalShot/styles.css";
 import "swiper/css/autoplay";
+import { useNavigate } from "react-router-dom";
 
 const TodayLiked = ({ userData }) => {
   const todayLikes = GetTodayLikes();
@@ -41,55 +44,94 @@ const TodayLiked = ({ userData }) => {
     }
   };
 
+  const navigate = useNavigate();
+  const onMoveToGoalShot = () => {
+    navigate("/goalshot");
+  };
+
   return (
-    <Swiper
-      slidesPerView={3}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination, Autoplay]}
-      autoplay={{ delay: 1000 }}
-      className="mySwiper"
-    >
-      {todayLikes !== undefined ? (
-        todayLikes?.map((v, i) => {
-          return (
-            <SwiperSlide key={i} onClick={() => onClickHandler(v.userId)}>
-              <StTodayLIked img={v.photoUrl} />
-            </SwiperSlide>
-          );
-        })
+    <>
+      {todayLikes?.length === 0 ? (
+        <StEmptyLiked onClick={onMoveToGoalShot}>
+          <span>좋아요를 누른 분들하고만</span>
+          <span>채팅이 가능해요!</span>
+          <span className="confirm">
+            좋아요를 누르러 가볼까요?
+            <HiCursorClick className="clickIcon" />
+          </span>
+        </StEmptyLiked>
       ) : (
-        <>
-          <SwiperSlide>
-            <StSkeleton />
-          </SwiperSlide>
-          <SwiperSlide>
-            <StSkeleton />
-          </SwiperSlide>
-          <SwiperSlide>
-            <StSkeleton />
-          </SwiperSlide>
-        </>
+        <Swiper
+          slidesPerView={3}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination, Autoplay]}
+          autoplay={{ delay: 1000 }}
+          className="mySwiper"
+        >
+          {todayLikes !== undefined ? (
+            todayLikes?.map((v, i) => {
+              return (
+                <SwiperSlide key={i} onClick={() => onClickHandler(v.userId)}>
+                  <StTodayLIked img={v.photoUrl} />
+                </SwiperSlide>
+              );
+            })
+          ) : (
+            <>
+              <SwiperSlide>
+                <StSkeleton />
+              </SwiperSlide>
+              <SwiperSlide>
+                <StSkeleton />
+              </SwiperSlide>
+              <SwiperSlide>
+                <StSkeleton />
+              </SwiperSlide>
+            </>
+          )}
+        </Swiper>
       )}
-    </Swiper>
+    </>
   );
 };
 
 export default TodayLiked;
 
-const StTodayLIked = styled.div`
+const StDupModel = styled.div`
   width: 100%;
   height: 100%;
   border-radius: 4px;
+`;
+
+const StTodayLIked = styled(StDupModel)`
   background-image: url(${(props) => props.img});
   background-position: center;
   background-size: cover;
 `;
 
-const StSkeleton = styled.div`
-  width: 100%;
-  height: 100%;
-  border-radius: 4px;
+const StSkeleton = styled(StDupModel)`
   background: var(--skeleton);
+`;
+
+const StEmptyLiked = styled(StDupModel)`
+  ${flex({ direction: "column" })}
+  width: 100vw;
+  height: 150px;
+  margin-top: 35px;
+  cursor: pointer;
+  span {
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 29px;
+    color: #956c4a;
+  }
+  .confirm {
+    margin-top: 10px;
+    font-size: 18px;
+    color: #292e41;
+  }
+  .clickIcon {
+  }
 `;
