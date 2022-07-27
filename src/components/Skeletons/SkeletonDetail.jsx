f9afa02d56c5000069c08813f30c4063de88b70f
baeg-jae <105036532+baeg-jae@users.noreply.github.com>
2022-07-27 20:34:41 +0900

    feat: SkeletonDetail 위치조정

--- src/components/Skeletons/SkeletonDetail.jsx
+++ src/components/Skeletons/SkeletonDetail.jsx
@@ -1,67 +1,88 @@
-import flex from "components/Common/flex";
-import styled from "@emotion/styled";
-import { SkeletonAnim } from "components/Common/GlobalStyles";
+import flex from 'components/Common/flex';
+import styled from '@emotion/styled';
+import { SkeletonAnim } from 'components/Common/GlobalStyles';
+import { MOBILE_SIZE_WIDTH } from 'shared/data';
 
 const SkeletonDetail = () => {
-  return (
-    <StWrapComment>
-      <StWrapFlex />
-      <StReplyWrap />
-      <StFlex>
-        <StProfile />
-        <StDiv />
-      </StFlex>
-    </StWrapComment>
-  );
+    return (
+        <StWrapComment>
+            <StDetail>
+                <StWrapFlex />
+                <StReplyWrap />
+                <StFlex>
+                    <StProfile />
+                    <StDiv />
+                </StFlex>
+            </StDetail>
+
+            <StNavBar />
+        </StWrapComment>
+    );
 };
 
 export default SkeletonDetail;
 
 const StWrapComment = styled.div`
-  ${flex({ direction: "column", justify: "flex-start" })}
-  width: 100%;
-  height: 100%;
-  margin-bottom: 160px;
+    ${flex({ direction: 'column' })}
+    width: 100%;
+    height: 100%;
+`;
+const StDetail = styled.div`
+    ${flex({ direction: 'column', justify: 'space-between' })}
+    width: 100%;
+    height: 100%;
 `;
 
 const StWrapFlex = styled.div`
-  ${flex({})}
-  width: calc(100% - 32px);
-  height: 440px;
-  margin-top: 59px;
-  border-radius: 4px;
-  background-color: var(--skeleton);
-  animation: ${SkeletonAnim} 0.5s linear infinite alternate;
+    ${flex({})}
+    width: calc(100% - 32px);
+    height: 440px;
+    margin-top: 59px;
+    border-radius: 4px;
+    background-color: var(--skeleton);
+    animation: ${SkeletonAnim} 0.5s linear infinite alternate;
 `;
 
 const StReplyWrap = styled.div`
-  width: 100%;
-  height: 170px;
-  margin: 10px;
-  margin-top: 35px;
+    position: fixed;
+    top: calc(100% - 150px);
+    width: calc(100% - 32px);
+    height: 92px;
 `;
 
 const StProfile = styled.div`
-  width: 40px;
-  height: 40px;
-  border-radius: 100%;
-  background-color: var(--skeleton);
-  animation: ${SkeletonAnim} 0.5s linear infinite alternate;
+    width: 40px;
+    height: 40px;
+    border-radius: 100%;
+    margin-left: 16px;
+    background-color: var(--skeleton);
+    animation: ${SkeletonAnim} 0.5s linear infinite alternate;
 `;
 
 const StDiv = styled.div`
-  ${flex({})}
-  width: calc(100% - 90px);
-  height: 40px;
-  margin-left: 12px;
-  border: 1px solid #bfbfbf;
-  border-radius: 55px;
-  background-color: var(--skeleton);
-  animation: ${SkeletonAnim} 0.5s linear infinite alternate;
+    ${flex({})}
+    width: calc(100% - 90px);
+    height: 40px;
+    margin-left: 12px;
+    border: 1px solid #bfbfbf;
+    border-radius: 55px;
+    background-color: var(--skeleton);
+    animation: ${SkeletonAnim} 0.5s linear infinite alternate;
 `;
 
 const StFlex = styled.div`
-  ${flex({})}
-  width: 100%;
-  margin-top: 30px;
+    ${flex({})}
+    width: 100%;
+    margin-bottom: 20px;
+    margin-right: 16px;
+`;
+
+const StNavBar = styled.div`
+    ${flex({ justify: 'space-evenly' })}
+    bottom: 0;
+    width: ${MOBILE_SIZE_WIDTH}px;
+    height: 68px;
+    @media screen and (max-width: 420px) {
+        width: 100%;
+    }
 `;
