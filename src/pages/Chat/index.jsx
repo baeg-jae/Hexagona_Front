fb56709c26f751485dfa68c7e6697ae8539d4764
baeg-jae <105036532+baeg-jae@users.noreply.github.com>
2022-07-27 19:00:52 +0900

    feat: 웰앱 다운기능

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,45 +1,46 @@
-import styled from "@emotion/styled";
-import { StWrap } from "components/Common/GlobalStyles";
-import { Helmet } from "react-helmet";
-import NavigatorBar from "components/Common/NavigatorBar";
-import Search from "components/Feed/Search";
-import TodayLiked from "components/Chat/TodayLiked";
-import useGetUser from "components/Hooks/User/useGetUser";
-import ChatList from "components/Chat/ChatList";
+import styled from '@emotion/styled';
+import { StWrap } from 'components/Common/GlobalStyles';
+import { Helmet } from 'react-helmet';
+import NavigatorBar from 'components/Common/NavigatorBar';
+import Search from 'components/Feed/Search';
+import TodayLiked from 'components/Chat/TodayLiked';
+import useGetUser from 'components/Hooks/User/useGetUser';
+import ChatList from 'components/Chat/ChatList';
 
 const Chat = () => {
-  const { data } = useGetUser();
-  return (
-    <StCalculatedWrap>
-      <Helmet>
-        <title>채팅: 갓생메이커</title>
-      </Helmet>
-      <>
-        <Search text="이름을 검색해주세요." />
-        <div style={{ width: "500px", height: "77px" }} />
-        <div>
-          <StLikeTitle>Goal Shot I Like</StLikeTitle>
-          <TodayLiked userData={data} />
-        </div>
-        <StLikeTitle>Chatting</StLikeTitle>
-        {/* props로 유저 정보를 넘겨줍니다. */}
-        <ChatList />
-      </>
-      <NavigatorBar />
-    </StCalculatedWrap>
-  );
+    const { data } = useGetUser();
+    return (
+        <StCalculatedWrap>
+            <Helmet>
+                <title>채팅: 갓생메이커</title>
+            </Helmet>
+            <>
+                <Search text="이름을 검색해주세요." />
+                <div style={{ width: '500px', height: '77px' }} />
+                <div>
+                    <StLikeTitle>Goal Shot I Like</StLikeTitle>
+                    <TodayLiked userData={data} />
+                </div>
+                <StLikeTitle>Chatting</StLikeTitle>
+                {/* props로 유저 정보를 넘겨줍니다. */}
+                <ChatList />
+            </>
+            <NavigatorBar />
+        </StCalculatedWrap>
+    );
 };
 
 export default Chat;
 
 const StCalculatedWrap = styled(StWrap)`
-  height: calc(100vh - 60px);
+    height: calc(100vh - 60px);
+    overflow-y: scroll;
 `;
 
 const StLikeTitle = styled.div`
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 28px;
-  color: #393939;
-  margin: 20px 0 0 19px;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 28px;
+    color: #393939;
+    margin: 20px 0 0 19px;
 `;
