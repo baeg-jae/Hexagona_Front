0c18573527b4861f4c95aefc542f27b87be63aad
baeg-jae <105036532+baeg-jae@users.noreply.github.com>
2022-07-28 07:42:19 +0900

    feat: a 태그 color 수정

--- src/components/SignIn/SocialButton.jsx
+++ src/components/SignIn/SocialButton.jsx
@@ -1,34 +1,33 @@
-import {
-  handleTextColor,
-  handleButtonColor,
-} from "components/Common/ButtonPropsHandler";
-import flex from "components/Common/flex";
-import styled from "@emotion/styled";
+import { handleTextColor, handleButtonColor } from 'components/Common/ButtonPropsHandler';
+import flex from 'components/Common/flex';
+import styled from '@emotion/styled';
 
 export const SocialButton = ({ link, text, name }) => {
-  return (
-    <StButtonDiv name={name}>
-      <a href={link}>
-        <span className="innerText">{text}</span>
-      </a>
-    </StButtonDiv>
-  );
+    return (
+        <StButtonDiv name={name}>
+            <a href={link}>
+                <span className="innerText">{text}</span>
+            </a>
+        </StButtonDiv>
+    );
 };
 
 const StButtonDiv = styled.div`
-  ${flex({ direction: "column" })}
-  width: 327px;
-  height: 56px;
-  border-radius: 10px;
-  margin-top: 8px;
-  background-color: ${(props) => handleButtonColor(props.name)};
-  color: ${(props) => handleTextColor(props.name)};
-  .innerText {
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 130%;
-  }
-  &:last-child {
-    margin-bottom: 30px;
-  }
+    ${flex({ direction: 'column' })}
+    width: 327px;
+    height: 56px;
+    border-radius: 10px;
+    margin-top: 8px;
+    background-color: ${(props) => handleButtonColor(props.name)};
+    a {
+        color: ${(props) => handleTextColor(props.name)};
+    }
+    .innerText {
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 130%;
+    }
+    &:last-child {
+        margin-bottom: 30px;
+    }
 `;
