3bdb87fdedae709f96b1e99a5ad8d192b9b3e308
baeg-jae <105036532+baeg-jae@users.noreply.github.com>
2022-07-28 17:27:07 +0900

    feat: feedDetiail input width update

--- src/components/Comment_Like/CommentInput.jsx
+++ src/components/Comment_Like/CommentInput.jsx
@@ -1,99 +1,108 @@
-import { COMMENT_MAX_LENGTH } from "shared/data";
-import { badWordsComment } from "shared/TextsData";
-import { useCallback, useState, useRef, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { CommentAddError } from "redux/modules/modal";
-import { BUTTON_CLICK_OFFSET } from "shared/data";
-import useAddComment from "components/Hooks/Comment/useAddComment";
-import useGetUser from "components/Hooks/User/useGetUser";
-import styled from "@emotion/styled";
-import flex from "../Common/flex";
-import { StInputDivGlobal } from "components/Common/GlobalStyles";
+import { COMMENT_MAX_LENGTH } from 'shared/data';
+import { badWordsComment } from 'shared/TextsData';
+import { useCallback, useState, useRef, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { CommentAddError } from 'redux/modules/modal';
+import { BUTTON_CLICK_OFFSET } from 'shared/data';
+import useAddComment from 'components/Hooks/Comment/useAddComment';
+import useGetUser from 'components/Hooks/User/useGetUser';
+import styled from '@emotion/styled';
+import flex from '../Common/flex';
+import { StInputDivGlobal, StWrap } from 'components/Common/GlobalStyles';
 
 const CommentInput = ({ postId }) => {
-  const { data } = useGetUser();
-  const [comment, setComment] = useState("");
-  const inputRef = useRef(null);
-  const buttonRef = useRef(null);
-  const [doubleClick, setDoubleClick] = useState(false);
-  const { mutate } = useAddComment();
-  const dispatch = useDispatch();
-  const addComment = useCallback(() => {
-    if (comment !== "") {
-      mutate({
-        comment: comment,
-        postId: postId,
-      });
-    } else {
-      dispatch(CommentAddError(true));
-    }
-    inputRef.current.value = "";
-  }, [comment, mutate, postId, dispatch]);
+    const { data } = useGetUser();
+    const [comment, setComment] = useState('');
+    const inputRef = useRef(null);
+    const buttonRef = useRef(null);
+    const [doubleClick, setDoubleClick] = useState(false);
+    const { mutate } = useAddComment();
+    const dispatch = useDispatch();
+    const addComment = useCallback(() => {
+        if (comment !== '') {
+            mutate({
+                comment: comment,
+                postId: postId,
+            });
+        } else {
+            dispatch(CommentAddError(true));
+        }
+        inputRef.current.value = '';
+    }, [comment, mutate, postId, dispatch]);
 
-  const bogusCheck = useCallback(() => {
-    buttonRef.current.disabled = true;
-    setDoubleClick(true);
-    const foundSwears = badWordsComment.filter((word) =>
-      comment.toLowerCase().includes(word.toLowerCase())
+    const bogusCheck = useCallback(() => {
+        buttonRef.current.disabled = true;
+        setDoubleClick(true);
+        const foundSwears = badWordsComment.filter((word) => comment.toLowerCase().includes(word.toLowerCase()));
+        if (foundSwears.length) {
+            dispatch(CommentAddError(true));
+        } else {
+            addComment();
+        }
+    }, [comment, addComment, dispatch]);
+
+    useEffect(() => {
+        // 버튼 더블클릭 방지
+        const interval = setTimeout(() => {
+            if (doubleClick) {
+                buttonRef.current.disabled = false;
+            }
+        }, `${BUTTON_CLICK_OFFSET}`);
+        return () => {
+            clearTimeout(interval);
+        };
+    }, [doubleClick]);
+
+    return (
+        <StCommentWrap>
+            <StWrapFlex>
+                <StProfile profile_img={data?.profile_img} />
+                <StInputDivGlobal>
+                    <input
+                        type="text"
+                        className="commentInput"
+                        placeholder="인증샷에 대한 감상평을 남겨주세요."
+                        onChange={(e) => setComment(e.target.value)}
+                        maxLength={COMMENT_MAX_LENGTH}
+                        ref={inputRef}
+                    />
+                    <button className="commentButton" onClick={bogusCheck} ref={buttonRef}>
+                        게시
+                    </button>
+                </StInputDivGlobal>
+            </StWrapFlex>
+        </StCommentWrap>
     );
-    if (foundSwears.length) {
-      dispatch(CommentAddError(true));
-    } else {
-      addComment();
-    }
-  }, [comment, addComment, dispatch]);
-
-  useEffect(() => {
-    // 버튼 더블클릭 방지
-    const interval = setTimeout(() => {
-      if (doubleClick) {
-        buttonRef.current.disabled = false;
-      }
-    }, `${BUTTON_CLICK_OFFSET}`);
-    return () => {
-      clearTimeout(interval);
-    };
-  }, [doubleClick]);
-
-  return (
-    <StWrapFlex>
-      <StProfile profile_img={data?.profile_img} />
-      <StInputDivGlobal>
-        <input
-          type="text"
-          className="commentInput"
-          placeholder="인증샷에 대한 감상평을 남겨주세요."
-          onChange={(e) => setComment(e.target.value)}
-          maxLength={COMMENT_MAX_LENGTH}
-          ref={inputRef}
-        />
-        <button className="commentButton" onClick={bogusCheck} ref={buttonRef}>
-          게시
-        </button>
-      </StInputDivGlobal>
-    </StWrapFlex>
-  );
 };
 
 export default CommentInput;
 
+const StCommentWrap = styled.div`
+    ${flex({
+        direction: 'column',
+        justify: 'space-between',
+    })}
+    width: calc(100% - 32px);
+    height: 100%;
+`;
+
 const StWrapFlex = styled.div`
-  ${flex({ justify: "flex-start" })}
-  position: fixed;
-  top: calc(100% - 150px);
-  width: calc(100% - 32px);
-  height: 92px;
-  background-color: var(--white);
-  @media screen and (min-width: 420px) {
-    width: 420px;
-  }
+    ${flex({})}
+    position: fixed;
+    top: calc(100% - 150px);
+    width: calc(100% - 32px);
+    height: 92px;
+    background-color: var(--white);
+    @media screen and (min-width: 400px) {
+        width: 400px;
+    }
 `;
 
 const StProfile = styled.div`
-  width: 40px;
-  height: 40px;
-  background-image: url(${(props) => props.profile_img});
-  background-position: center;
-  background-size: cover;
-  border-radius: 100%;
+    width: 40px;
+    height: 40px;
+    background-image: url(${(props) => props.profile_img});
+    background-position: center;
+    background-size: cover;
+    border-radius: 100%;
 `;
