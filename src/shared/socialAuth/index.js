919bdcda1c757fdcaf9ca7dd784ed7ac9519dc00
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-29 13:13:45 -0400

    feat: redirect

--- src/shared/socialAuth/index.js
+++ src/shared/socialAuth/index.js
@@ -1,18 +1,20 @@
 // GOOGLE
 const GOOGLE_REACT_APP_CLIENTID = process.env.REACT_APP_CLIENTID;
 const GOOGLE_REACT_APP_REDIRECT_URI =
-  "http://godlife.co.kr/oauth/google/callback";
+  "http://01055858456.s3-website.ap-northeast-2.amazonaws.com/oauth/google/callback";
 export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_REACT_APP_CLIENTID}&redirect_uri=${GOOGLE_REACT_APP_REDIRECT_URI}&response_type=code&scope=email%20profile%20openid&access_type=offline`;
 
 // KAKAO
 const KAKAO_REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-const KAKAO_REDIRECT_URI = "http://godlife.co.kr/oauth/kakao/callback";
+const KAKAO_REDIRECT_URI =
+  "http://01055858456.s3-website.ap-northeast-2.amazonaws.com/oauth/kakao/callback";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
 //Naver
 const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENTID;
-const NAVER_REDIRECT_URI = "http://godlife.co.kr/oauth/naver/callback";
+const NAVER_REDIRECT_URI =
+  "http://01055858456.s3-website.ap-northeast-2.amazonaws.com/oauth/naver/callback";
 
 const STATE_STRING = process.env.REACT_APP_STATE_STRING;
 
