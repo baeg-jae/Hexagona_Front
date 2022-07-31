c31dd211a2c234bf35194da42c0775107d7fef87
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-31 15:50:20 -0400

    fix Goal Shot Page Responsive

--- src/components/GoalShot/GoalShotCards.jsx
+++ src/components/GoalShot/GoalShotCards.jsx
@@ -202,6 +202,9 @@ const StCard = styled.div`
     animation: ${(props) => props.flag && CenterCardAnimRes()}
       ${CARD_ANIMATION_TIME}s ease;
   }
+  @media screen and (max-width: 320px) {
+    min-width: 250px;
+  }
   @media screen and (max-height: 700px) {
     height: 350px;
     margin-top: ${(props) => (props.differ ? "120px" : "68px")};
@@ -301,7 +304,7 @@ const StRightRightCard = styled(StCard)`
 
 const StLastCard = styled.div`
   ${flex({ direction: "column", justify: "flex-start" })}
-  min-width: 285px;
+  max-width: 312px;
   height: 480px;
   border-radius: 20px;
   opacity: ${(props) => (props.differ ? "0.6" : "1")};
@@ -312,6 +315,8 @@ const StLastCard = styled.div`
   background: ${(props) =>
     props.bg ? "linear-gradient(0deg, #f9f9f9, #f9f9f9)" : "none"};
   color: var(--white);
+  overflow: hidden !important;
+
   .innerTextDiv {
     ${flex({ direction: "column" })}
     margin-top: 28px;
@@ -321,18 +326,17 @@ const StLastCard = styled.div`
     font-size: 24px;
   }
   .smallText {
+    font-family: Pretendard;
+    font-weight: 400;
     font-size: 14px;
-    margin-top: 20px;
+    margin-top: 29px;
+    margin-bottom: 32px;
   }
   .likedPicsDiv {
-    width: 300px;
+    width: 388px;
   }
-  @media screen and (max-height: 820px) {
-    height: 500px;
-
-    .likedPicsDiv {
-      width: 300px;
-    }
+  @media screen and (max-height: 845px) {
+    height: 400px;
   }
   @media screen and (max-height: 700px) {
     height: 350px;
@@ -342,5 +346,13 @@ const StLastCard = styled.div`
     .likedPicsDiv {
       width: 260px;
     }
+  }
+  @media screen and (max-width: 380px) {
+    .likedPicsDiv {
+      width: 390px;
+    }
+  }
+  @media screen and (max-width: 320px) {
+    max-width: 250px;
   }
 `;
