0a29c1d8cebe0f2e724793e9aeeba66538f4caf0
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-08-01 14:12:34 -0400

    feat: Back Button in Detail

--- src/pages/FeedDetail/index.jsx
+++ src/pages/FeedDetail/index.jsx
@@ -9,6 +9,7 @@ import flex from "components/Common/flex";
 import styled from "@emotion/styled";
 import WebTitle from "components/Common/WebTitle";
 import Loading from "pages/Status/Loading";
+import BackButton from "components/Common/BackButton";
 
 const FeedDetail = () => {
   const { postId } = useParams();
@@ -24,7 +25,11 @@ const FeedDetail = () => {
         <>
           <StWrapComment>
             <WebTitle text="피드: 갓생메이커" />
-            hi
+            <StHeader>
+              <BackButton />
+              <StOtherName>피드</StOtherName>
+              <span style={{ opacity: "0", marginRight: "24px" }}>.</span>
+            </StHeader>
             <CommentImg
               category={data?.category}
               postContent={data?.postContent}
@@ -59,4 +64,23 @@ const StWrapComment = styled.div`
   overflow-y: scroll;
   overflow-x: hidden;
   margin-bottom: 160px;
+`;
+
+const StHeader = styled.div`
+  ${flex({ justify: "space-between" })}
+  position: fixed;
+  background-color: var(--white);
+  width: 100%;
+  max-width: 420px;
+  height: 56px;
+  border-bottom: 1px solid #f2f2f2;
+  margin-bottom: 18px;
+`;
+
+const StOtherName = styled.span`
+  font-weight: 700;
+  font-family: Pretendard_Bold;
+  font-size: 14px;
+  line-height: 17px;
+  color: #393939;
 `;
