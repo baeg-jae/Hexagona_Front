fd3eb67f01299667a1a215abc5778199d74e4309
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-08-04 00:57:56 -0400

    fix: redirect

--- src/shared/socialAuth/index.js
+++ src/shared/socialAuth/index.js
@@ -3,7 +3,7 @@ const GOOGLE_REACT_APP_CLIENTID = process.env.REACT_APP_CLIENTID;
 // const GOOGLE_REACT_APP_REDIRECT_URI =
 //   "http://godlife.co.kr/oauth/google/callback";
 const GOOGLE_REACT_APP_REDIRECT_URI =
-  "http://localhost:3000/oauth/google/callback";
+  "hhttps://www.godlife.co.kr/oauth/google/callback";
 
 export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_REACT_APP_CLIENTID}&redirect_uri=${GOOGLE_REACT_APP_REDIRECT_URI}&response_type=code&scope=email%20profile%20openid&access_type=offline`;
 
@@ -17,7 +17,7 @@ export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id
 //Naver
 const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENTID;
 // const NAVER_REDIRECT_URI = "http://godlife.co.kr/oauth/naver/callback";
-const NAVER_REDIRECT_URI = "http://localhost:3000/oauth/naver/callback";
+const NAVER_REDIRECT_URI = "https://www.godlife.co.kr/oauth/naver/callback";
 
 const STATE_STRING = process.env.REACT_APP_STATE_STRING;
 
