b0e1e6154a775143f64729c984203ef11808a417
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-08-04 02:49:32 -0400

    fix:redirect

--- src/pages/LogIn/index.jsx
+++ src/pages/LogIn/index.jsx
@@ -44,14 +44,14 @@ const LogIn = () => {
         <SocialButton
           link={NAVER_AUTH_URL}
           name="naver"
-          // text="네이버 계정으로 시작하기"
-          text="긴급수정 중입니다."
+          text="네이버 계정으로 시작하기"
+          // text="긴급수정 중입니다."
         />
         <SocialButton
           link={GOOGLE_AUTH_URL}
           name=""
-          // text="Google 계정으로 계속하기"
-          text="긴급수정 중입니다."
+          text="Google 계정으로 계속하기"
+          // text="긴급수정 중입니다."
         />
       </div>
     </StFirstPageDiv>
